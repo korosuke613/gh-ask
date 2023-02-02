f323b7d6c312ec728bac1a3323387db9e0c89f0a
Futa HIRAKOBA <korosuke613613@gmail.com>
2023-02-02 11:11:38 +0900

    Step 3: Talking to the API

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func cli() error {
 		repo, err = repository.Parse(*repoOverride)
 	}
 	if err != nil {
-		return fmt.Errorf("could not determine what repo to use: %v", err.Error())
+		return fmt.Errorf("could not determine what repo to use: %w", err)
 	}
 
 	if len(flag.Args()) < 1 {
@@ -40,7 +40,63 @@ func cli() error {
 	}
 	search := strings.Join(flag.Args(), " ")
 
-	fmt.Printf("Going to search discussions in '%s/%s' for '%s'\n", repo.Owner(), repo.Name(), search)
+	client, err := gh.GQLClient(nil)
+	if err != nil {
+		return fmt.Errorf("could not create a graphql client: %w", err)
+	}
+
+	query := fmt.Sprintf(`{
+		repository(owner: "%s", name: "%s") {
+			hasDiscussionsEnabled
+			discussions(first: 100) {
+				edges { node {
+					title
+					body
+					url
+		}}}}}`, repo.Owner(), repo.Name())
+
+	type Discussion struct {
+		Title string
+		URL   string `json:"url"`
+		Body  string
+	}
+
+	response := struct {
+		Repository struct {
+			Discussions struct {
+				Edges []struct {
+					Node Discussion
+				}
+			}
+			HasDiscussionsEnabled bool
+		}
+	}{}
+
+	err = client.Do(query, nil, &response)
+	if err != nil {
+		return fmt.Errorf("failed to talk to the GitHub API: %w", err)
+	}
+
+	if !response.Repository.HasDiscussionsEnabled {
+		return fmt.Errorf("%s/%s does not have discussions enabled", repo.Owner(), repo.Name())
+	}
+
+	var matches []Discussion
+
+	for _, edge := range response.Repository.Discussions.Edges {
+		if strings.Contains(edge.Node.Body+edge.Node.Title, search) {
+			matches = append(matches, edge.Node)
+		}
+	}
+
+	if len(matches) == 0 {
+		fmt.Fprintln(os.Stderr, "No matching discussion threads found :(")
+		return nil
+	}
+
+	for _, d := range matches {
+		fmt.Printf("%s %s\n", d.Title, d.URL)
+	}
 
 	return nil
 }
