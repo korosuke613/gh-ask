1852de7de2ac779fe0c1eeaec61cfd45c1e7c192
Futa HIRAKOBA <korosuke613613@gmail.com>
2023-02-02 10:54:59 +0900

    Step 1: Selecting a repository

--- main.go
+++ main.go
@@ -1,25 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/cli/go-gh"
+	"github.com/cli/go-gh/pkg/repository"
 )
 
 func main() {
-	fmt.Println("hi world, this is the gh-ask extension!")
-	client, err := gh.RESTClient(nil)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if err := cli(); err != nil {
+		fmt.Fprintf(os.Stderr, "gh-ask failed: %s\n", err.Error())
+		os.Exit(1)
 	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
+}
+
+func cli() error {
+	repoOverride := flag.String(
+		"repo", "", "Specify a repository. If omitted, uses current repository")
+	flag.Parse()
+
+	var repo repository.Repository
+	var err error
+
+	if *repoOverride == "" {
+		repo, err = gh.CurrentRepository()
+	} else {
+		repo, err = repository.Parse(*repoOverride)
 	}
-	fmt.Printf("running as %s\n", response.Login)
+	if err != nil {
+		return fmt.Errorf("could not determine what repo to use: %v", err.Error())
+	}
+
+	fmt.Printf("Going to search discussions in %s/%s\n", repo.Owner(), repo.Name())
+
+	return nil
 }
 
 // For more examples of using go-gh, see:
